8df85e814ea957d965c6d61fd6b862c020c6da30
somebody3232 <samuelshuster@gmail.com>
2023-12-30 18:16:01 -0500

    Improve +page and edit .cpanel.yml

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<div class="w-full h-[100vh] flex items-center justify-center">
+<div class="w-full h-[100vh] flex items-center justify-center flex-col">
     <h1 class="text-6xl font-black">When Does This Period End?</h1>
-    <p>⚠️️️WIP⚠️</p>
+    <p class="text-3xl m-10">⚠️️️WIP⚠️</p>
+    <p class="text-3xl -mt-10">Coming Soon!</p>
 </div>
