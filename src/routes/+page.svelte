6b0f39d2131c8d3b21f9a516de04edca2d1b4cac
somebody3232 <samuelshuster@gmail.com>
2023-12-30 23:39:01 -0500

    Dark Mode support (full push test)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-full h-[100vh] flex items-center justify-center flex-col">
+<div class="w-full h-[100vh] flex items-center justify-center flex-col dark:bg-gray-800 dark:text-white">
     <h1 class="text-6xl font-black">When Does This Period End?</h1>
     <p class="text-3xl m-10">⚠️️️WIP⚠️</p>
     <p class="text-3xl -mt-10">Coming Soon!</p>
