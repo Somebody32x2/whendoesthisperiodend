6108406ad273751dc9e4d2c621575a19f050bbac
Somebody32x2 <samuelshuster@gmail.com>
2024-01-20 19:53:46 -0500

    Schedule constructor + one Schedule + RangesProgressBar.ts + various Utils

--- src/lib/Schedule.ts
+++ src/lib/Schedule.ts
@@ -1,66 +1,150 @@
-import type {Period} from "$lib/Utils";
-import { DateTime, Interval, type WeekdayNumbers} from "luxon";
-
+import {lastWeekday, nextWeekday, type Period} from "$lib/Utils";
+import {DateTime, Interval, type WeekdayNumbers} from "luxon";
+import type {ProgressBar} from "$lib/ProgressBar";
 
 interface Schedule {
     label: String;
     periods: Period[];
 }
-interface NormalSchedule extends Schedule {
+// Represents a normal schedule for a certain day or days of the week. Must have at least one period.
+class NormalSchedule implements Schedule {
     label: String;
     days: WeekdayNumbers[]
-    periods: Period[];
-}
+    periods: [Period, ...Period[]]; // Array must have at least one period
+    endWithWeekend: boolean
+    constructor(label: String, days: WeekdayNumbers[], periods: [Period, ...Period[]], endWithWeekend=false) {
+        this.label = label;
+        this.days = days;
+        this.periods = periods;
+        this.endWithWeekend = endWithWeekend;
+    }
 
+
+}
+// Represents a special schedule for a certain day or days of the year, such as an exam schedule. Must have at least one period.
 interface SpecialSchedule extends Schedule {
     label: String;
-    periods: Period[];
+    periods: [Period, ...Period[]]; // Array must have at least one period
     daysApplicable: DateTime[];
 }
-interface Break {
+interface Break extends Schedule {
     label: String;
     interval: Interval;
+    periods: []
 }
-const BREAKSCHEDULE: Schedule = {
-    label: "Break",
-    periods: [],
+interface NormalWeekendConfig {
+    startDay: WeekdayNumbers
+    endDay: WeekdayNumbers
+    startTime: DateTime
+    endTime: DateTime
 }
-class FullSchedule {
+export class FullSchedule {
     // normalSchedules: NormalSchedule[] // Friday, Weekday
     // specialSchedules: SpecialSchedule[] // 2Exam, 1Exam
     // breaks: Break[] // Spring Break, Winter Break, Thanksgiving Break, etc.
 
     todaySchedule: Schedule;
-    constructor(normalSchedules: NormalSchedule[], specialSchedules: SpecialSchedule[], breaks: Break[]) {
+    endOfDay: Break;
+    additonalBars: ProgressBar[];
+    constructor(normalSchedules: NormalSchedule[], specialSchedules: SpecialSchedule[], breaks: Break[], normalWeekendConfig: NormalWeekendConfig, additionalBars: ProgressBar[]) {
         // this.normalSchedules = normalSchedules;
         // this.specialSchedules = specialSchedules;
         // this.breaks = breaks;
+        function findSchedule(time: DateTime = DateTime.now()): {todaySchedule: Schedule, endOfDay: Break} {
+            let foundSchedule = false;
+            let todaySchedule: Schedule;
+            let endOfDay: Break;
+            // Check if we are in a break
+            for (const break_ of breaks) {
+                if (break_.interval.contains(time)) {
+                    todaySchedule = break_;
+                    endOfDay = break_;
+                    foundSchedule = true;
+                }
+            }
+            // Check if we are in a special schedule
+            if (!foundSchedule) for (const specialSchedule of specialSchedules) {
+                for (const day of specialSchedule.daysApplicable) {
+                    if (day.hasSame(time, "day")) {
+                        todaySchedule = specialSchedule;
+                        // end of day is the break to next day unless it has no periods in which case it is the normal weekend
+                        let tmwSchedule = findSchedule(time.plus({days: 1}));
+                        if (tmwSchedule.todaySchedule.periods.length === 0) {
+                            endOfDay = {
+                                label: "Weekend",
+                                interval: Interval.fromDateTimes(
+                                    lastWeekday(normalWeekendConfig.startDay, DateTime.now(), normalWeekendConfig.startTime),
+                                    nextWeekday(normalWeekendConfig.endDay, DateTime.now(), normalWeekendConfig.endTime)
+                                ),
+                                periods: []
+                            }
+                        } else {
+                            endOfDay = {
+                                label: "Tomorrow",
+                                interval: Interval.fromDateTimes(
+                                    specialSchedule.periods[specialSchedule.periods.length].end,
+                                    tmwSchedule.todaySchedule.periods[0].start
+                                ),
+                                periods: []
+                            };
+                        }
+                        foundSchedule = true;
+                    }
+                }
+            }
+            // Check if we are in a normal schedule
+            if (!foundSchedule) for (const normalSchedule of normalSchedules) {
+                for (const day of normalSchedule.days) {
+                    if (day === time.weekday) {
+                        todaySchedule = normalSchedule;
+                        if (normalSchedule.endWithWeekend) {
+                            // end of day is the next weekend
+                            endOfDay = {
+                                label: "Weekend",
+                                interval: Interval.fromDateTimes(
+                                    nextWeekday(normalWeekendConfig.startDay, DateTime.now(), normalWeekendConfig.startTime),
+                                    nextWeekday(normalWeekendConfig.endDay, DateTime.now(), normalWeekendConfig.endTime)
+                                ),
+                                periods: []
+                            }
+                        } else {
+                            // end of day is the next day
+                            endOfDay = {
+                                label: "Tomorrow",
+                                interval: Interval.fromDateTimes(
+                                    normalSchedule.periods[normalSchedule.periods.length].end,
+                                    findSchedule(time.plus({days: 1})).todaySchedule.periods[0].start
+                                ),
+                                periods: []
+                            }
+                        }
+                        foundSchedule = true;
+                    }
+                }
+            }
+            // Check if we are in a weekend
+            if (!foundSchedule) {
+                todaySchedule = {
+                    label: "Weekend",
+                    periods: [],
+                }
+                endOfDay = {
+                    label: "Weekend",
+                    interval: Interval.fromDateTimes(
+                        lastWeekday(normalWeekendConfig.startDay, DateTime.now(), normalWeekendConfig.startTime),
+                        nextWeekday(normalWeekendConfig.endDay, DateTime.now(), normalWeekendConfig.endTime)
+                    ),
+                    periods: []
+                }
+            }
+            // @ts-ignore we know todaySchedule and endOfDay are defined
+            return {todaySchedule, endOfDay}
+        }
 
-        // Check if we are in a break
-        for (const break_ of breaks) {
-            if (break_.interval.contains(DateTime.now())) {
-                this.todaySchedule = BREAKSCHEDULE;
-                return;
-            }
-        }
-        // Check if we are in a special schedule
-        for (const specialSchedule of specialSchedules) {
-            for (const day of specialSchedule.daysApplicable) {
-                if (day.hasSame(DateTime.now(), "day")) {
-                    this.todaySchedule = specialSchedule;
-                    return;
-                }
-            }
-        }
-        // Check if we are in a normal schedule
-        for (const normalSchedule of normalSchedules) {
-            for (const day of normalSchedule.days) {
-                if (day === DateTime.now().weekday) {
-                    this.todaySchedule = normalSchedule;
-                    return;
-                }
-            }
-        }
-        throw new Error("No schedule found for today");
+        let fullSchedule = findSchedule(DateTime.now());
+        this.todaySchedule = fullSchedule.todaySchedule;
+        this.endOfDay = fullSchedule.endOfDay;
+
+        this.additonalBars = additionalBars;
     }
 }
---
+++ src/lib/WS.ts
@@ -0,0 +1,68 @@
+import {FullSchedule} from "$lib/Schedule";
+import {nthString, Period, Time} from "$lib/Utils";
+import {DateTime, Interval} from "luxon";
+import {RangesProgressBar} from "$lib/RangesProgressBar";
+import {StaticProgressBar} from "$lib/StaticProgressBar";
+
+export let fullSchedule = new FullSchedule(
+    // Normal Schedules
+    [
+        {
+            label: "Weekday",
+            days: [1, 2, 3, 4],
+            periods: [{"start": Time("8:30"), end: Time("9:19"), "label": "1st Period"}, // Add 1st period to satisfy the "at least one period" requirement
+                ...[["9:23", "10:12"], ["10:16", "11:05"], ["11:09", "12:02"], ["12:55", "13:44"], ["13:48", "14:37"], ["14:41", "15:30"]]
+                    .map((value, index) => new Period(Time(value[0]), Time(value[1]), `${nthString(index + 2)} Period`))],
+            endWithWeekend: false
+        }
+    ],
+    // Special Schedules
+    [
+        {
+            "label": "One Exam",
+            "daysApplicable": [],
+            "periods": [{
+                "start": Time("8:30"),
+                "end": Time("10:30"),
+                "label": "1st Period Exam"
+            }, {"start": Time("10:35"), "end": Time("12:35"), "label": "2nd Period Exam"}] // TODO: Fix this
+        }
+    ],
+    // Breaks
+    [
+        {
+            label: "President's Day Weekend",
+            periods: [],
+            interval: Interval.fromDateTimes(DateTime.fromISO("2024-02-16T14:15"), DateTime.fromISO("2024-02-19T23:59"))
+        },
+        {
+            label: "Long Weekend - Student Holiday",
+            periods: [],
+            interval: Interval.fromDateTimes(DateTime.fromISO("2024-03-14T15:30"), DateTime.fromISO("2024-03-17T23:59"))
+        },
+        {
+            label: "Spring Break",
+            periods: [],
+            interval: Interval.fromDateTimes(DateTime.fromISO("2024-03-22T14:15"), DateTime.fromISO("2024-03-31T23:59"))
+        },
+        // {
+        //     label: "Memorial Day Weekend",
+        //     periods: [],
+        //     interval: Interval.fromDateTimes(DateTime.fromISO("2024-05-24T14:15"), DateTime.fromISO("2024-05-27T23:59"))
+        // }
+    ], {
+        startDay: 5,
+        endDay: 1,
+        startTime: Time("2:15"),
+        endTime: Time("8:30")
+    },
+    [
+        new RangesProgressBar("quarters", [
+            [DateTime.fromISO("2024-01-08T15:30"), DateTime.fromISO("2024-03-13T15:30")],
+            [DateTime.fromISO("2024-03-14T8:30"), DateTime.fromISO("2024-05-24T14:15")],
+        ], ["3rd Quarter", "4th Quarter"], true, "grape"),
+        new RangesProgressBar("semesters", [
+            [DateTime.fromISO("2024-01-08T15:30"),  DateTime.fromISO("2024-05-24T14:15")],
+        ], ["2nd Semester"], true, "dark"),
+        new StaticProgressBar("year", "The School Year", "2023-08-10T8:30", "2024-05-24T14:15", true, "lime"),
+    ])
