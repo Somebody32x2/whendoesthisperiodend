6f82c08a53749aa894c7a9ed5cbf95bba1181935
Somebody32x2 <samuelshuster@gmail.com>
2025-12-02 21:06:31 -0500

    Try to make it build properly

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import type {HandleServerError} from '@sveltejs/kit';
-
-export const handleError: HandleServerError = ({error, event}) => {
-    if (error instanceof Error && error.message.includes('Not found')) { // ignore 404s
-        return; // Prevent SvelteKit's default error handling for this specific error
-    }
-    // Let SvelteKit handle other errors normally
-    return error;
-};
